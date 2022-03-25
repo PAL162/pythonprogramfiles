d225dd42a504ce5f01db19728f6dbebc1b0cb9e9
PAL162 <96531335+PAL162@users.noreply.github.com>
2022-03-25 21:54:40 +0530

    Add files via upload

---
+++ practical10.py
@@ -0,0 +1,14 @@
+# Generate PDF file of your 3rd Semester Mark sheet
+import img2pdf
+from PIL import Image
+import os
+img_path = 'D:sm6result.jpg'
+pdf_path = 'D:sm6result.pdf'
+image = Image.open(img_path)
+pdf_bytes = img2pdf.convert(image.filename)
+file = open(pdf_path, "wb")
+file.write(pdf_bytes)
+image.close()
+file.close()
+print("successful create pdf file.")
+#Khant Pal_D21CE162
